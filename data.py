77d0becc3cbb73686118894734e648b3156cee61
Benson <156505675+Ben-the-Analyst@users.noreply.github.com>
2024-10-28 18:55:19 +0300

    Update data.py Daily data

--- data.py
+++ data.py
@@ -8,22 +8,21 @@ def show_data_page():
     tab = st.tabs(["Route Plans", "Daily Data", "HCP Data"])
 
     with tab[0]:
-        st.subheader("Filtered Product Data")
+        # st.subheader("Filtered Product Data")
 
-        st.title("Read Google Sheet as DataFrame")
-
-        conn = st.connection("gsheets", type=GSheetsConnection)
-        df = conn.read(worksheet="RoutePlanner")
+        # st.title("Read Google Sheet as DataFrame")
 
         conn = st.connection("gsheets", type=GSheetsConnection)
-        # df2 = conn.read(worksheet="Institutions")
+        Route_data = conn.read(worksheet="RoutePlanner")
 
-        st.dataframe(df)
-        # st.dataframe(df2)
+        st.dataframe(Route_data)
 
     with tab[1]:
-        st.subheader("Summary Statistics")
+
+        conn = st.connection("gsheets", type=GSheetsConnection)
+        Daily_data = conn.read(worksheet="DailyData")
+        st.dataframe(Daily_data)
 
     with tab[2]:
         # bar chart
-        st.subheader("Nitrogen Ingredients in Products")
+        st.title("HCP Data")
