9a681cf33502bf2e45f420daa7642bec8dfb4013
Benson <156505675+Ben-the-Analyst@users.noreply.github.com>
2024-10-28 19:01:09 +0300

    hcp data.py data read

--- data.py
+++ data.py
@@ -8,6 +8,7 @@ def show_data_page():
     tab = st.tabs(["Route Plans", "Daily Data", "HCP Data"])
 
     with tab[0]:
+        # Route Plans
         # st.subheader("Filtered Product Data")
 
         # st.title("Read Google Sheet as DataFrame")
@@ -18,11 +19,14 @@ def show_data_page():
         st.dataframe(Route_data)
 
     with tab[1]:
-
+        # Daily Data
         conn = st.connection("gsheets", type=GSheetsConnection)
         Daily_data = conn.read(worksheet="DailyData")
         st.dataframe(Daily_data)
 
     with tab[2]:
-        # bar chart
-        st.title("HCP Data")
+        # HCP
+        # st.title("HCP Data")
+        conn = st.connection("gsheets", type=GSheetsConnection)
+        HCP_data = conn.read(worksheet="HCPData")
+        st.dataframe(HCP_data)
